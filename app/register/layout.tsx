5cbd13691220cd4b80882a29b0912ab596fcbc2a
Damilola Fajorin <30724826+philiphbk@users.noreply.github.com>
2024-01-06 23:15:08 +0100

    creating folder stucture

---
+++ app/register/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Kids Pickup App',
+  description: 'a simple app to help parents organize and monitor pickups',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
