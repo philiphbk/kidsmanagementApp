56eb5fa4d3bc583c68cac9c90f12aad14d8bfaeb
Damilola Fajorin <30724826+philiphbk@users.noreply.github.com>
2024-01-13 23:49:53 +0100

    added button, tick and style

--- app/register/loading.tsx
+++ app/register/loading.tsx
@@ -1,5 +1,11 @@
+import HODLogo from "@/public/images/hodlogo1.png";
+import Image from "next/image";
+
 export default function Loading() {
   return (
-    <div>loading...</div>
+    <div className=" flex flex-col items-center my-32 p-24">
+      <Image src={HODLogo} width={100} height={100} alt="hodlogo" />
+      loading...
+    </div>
   )
 }
--- app/register/success/page.tsx
+++ app/register/success/page.tsx
@@ -1,19 +1,37 @@
 import Image from "next/image";
 import HODLogo from "@/public/images/hodlogo1.png";
+import Tick from "@/public/images/Circle.png";
+
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const meta: Metadata = {
+  title: "Success Registration",
+  description: "Registration successful",
+  icons: "@/public/images/hodlogo1.png",
+};
 
 export default function Success() {
   return (
     <>
-      <div className=" flex flex-col p-24 items-center">
+      <div className=" success_container flex flex-col p-24 items-center">
         <Image src={HODLogo} width={80} height={80} alt="hodlogo" />
-        <p>HOD Kids Pick-Up Platform</p>
+        <p className=" mb-4">HOD Kids Pick-Up Platform</p>
         <div className=" flex flex-col items-center border rounded p-4 shadow-xl">
-          <h1>Registration Successful</h1>
+          <Image src={Tick} width={100} height={100} alt="tick" />
+          <h1 className=" font-bold">Registration Successful</h1>
           <p>Voila! There you go!</p>
           <br />
           <hr></hr>
           <br />
-          <p>Thank you for registering your child(ren) with us!</p>
+          <p className=" mb-14">
+            Thank you for registering your child(ren) with us!
+          </p>
+          <Link href={`/register`}>
+            <button className=" bg-[#273472] rounded px-4 py-5 text-wrap text-white font-semibold">
+              Go to Home
+            </button>
+          </Link>
         </div>
       </div>
     </>
