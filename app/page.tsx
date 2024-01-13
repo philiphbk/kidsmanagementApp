93371acd0d1d2b451744c7b4ffbc2c9d4d838495
Damilola Fajorin <30724826+philiphbk@users.noreply.github.com>
2024-01-13 20:47:25 +0100

    added register main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-40">
       <h1>Welcome to Kid Management App</h1>
 
         <Link href="/register">
