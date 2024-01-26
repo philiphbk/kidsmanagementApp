fe1e89881da15235225fa7e629e14136b0c1fdc1
Damilola Fajorin <30724826+philiphbk@users.noreply.github.com>
2024-01-26 14:09:34 +0100

    Corrected errors

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,36 +2,37 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 import User from "../../../lib/definitions/user";
+import { verifyToken } from "../../../lib/verifyToken";
 
 export default NextAuth({
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
         
-        email: { label: "Email", type: "email", placeholder: "Email" },
-      },
-      authorize: async (credentials) => {
-        const user = await User.findOne({
-          where: { email: credentials?.email },
-        });
+    //     email: { label: "Email", type: "email", placeholder: "Email" },
+    //   },
+    //   authorize: async (credentials) => {
+    //     const user = await User.findOne({
+    //       where: { email: credentials?.email },
+    //     });
 
-        if (user) {
-          return { id: user.id.toString(), name: user.name, email: user.email };
-        } else {
-          throw new Error("No user found");
-        }
-      },
-    }),
+    //     if (user) {
+    //       return { id: user.id.toString(), name: user.name, email: user.email };
+    //     } else {
+    //       throw new Error("No user found");
+    //     }
+    //   },
+    // }),
     CredentialsProvider({
       name: "Magic Link",
       credentials: {
-        token: { label: "Token", type: "text" }
+        token: { label: "Token", type: "string" }
       },
       authorize: async (credentials) => {
-        const user = await verifyToken(credentials.token);
+        const user = await verifyToken(credentials?.token ?? '');
         if (user) {
-          return user; // Return user object on successful verification
+          return { ...user, id: user.id.toString() }; // Convert id to string
         } else {
           throw new Error('Invalid token'); // Throw error on failure
         }
@@ -39,13 +40,15 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async jwt(token, user) {
+    async jwt(params: { token: any; user: any }) {
+      const { token, user } = params;
       if (user) {
         token.id = user.id;
       }
       return token;
     },
-    async session(session, token) {
+    async session(params: { session: any; token: any; trigger: "update" | "signout" }) {
+      const { session, token } = params;
       session.user.id = token.id;
       return session;
     }
