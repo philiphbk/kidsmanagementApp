23a6c7997164617fcff7b6bae8cc0c5214755f79
Nte Matthew <nte.matthew@saltpay.co>
2024-03-24 08:38:46 +0100

    add email login for parent

--- pages/api/config/index.ts
+++ pages/api/config/index.ts
@@ -1,5 +1,10 @@
+import 'dotenv/config';
+
 const config = {
-  SECRET_KEY: 'wowthisisabadsecret12345'
+  SECRET_KEY: 'wowthisisabadsecret12345',
+  EMAIL_OPTIONS: {
+    from: process.env.EMAIL_FROM || 'philip@gmail.com',
+  }
 }
 
 export default config
--- pages/api/parents/service.ts
+++ pages/api/parents/service.ts
@@ -1,4 +1,7 @@
 import { Parent } from "@/lib/definitions/form-interfaces";
+import config from "../config";
+import { db } from "../db";
+import { generateMagicLink, sendEmail } from "../login/sendEmail";
 import { makeParent } from "../models";
 
 export const createParent = async (parentData: Parent) => {
@@ -7,3 +10,19 @@ export const createParent = async (parentData: Parent) => {
   await parent.save(parentDataToSave);
 }
 
+export const loginParent = async (email: string, password: string) => {
+  const parent = await db("parent").getByEmail(email);
+  if (!parent) {
+    throw new Error("User not found");
+  }
+  const loginLink = await generateMagicLink(email);
+  const options = {
+    from: config.EMAIL_OPTIONS.from,
+    to: email,
+    subject: "Your Magic Login Link",
+    html: `<p>Click <a href="${loginLink}">here</a> to log in.</p>`,
+  }
+
+  await sendEmail(options)
+  return "Email sent";
+}
