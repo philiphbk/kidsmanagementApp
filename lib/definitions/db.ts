0b03b95d3395f572380192a515fbc8e5849785b1
Damilola Fajorin <30724826+philiphbk@users.noreply.github.com>
2024-03-31 10:12:03 +0100

    update

--- lib/definitions/db.ts
+++ lib/definitions/db.ts
@@ -2,6 +2,8 @@ import { CaregiverForm, ChildForm, ParentForm } from "./form-interfaces";
 
 type Model = ChildForm | ParentForm | CaregiverForm;
 
+// test
+
 export interface DBModel {
   (tableName: string): {
     getOne: (id: string) => Promise<any>;
